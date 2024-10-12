45506be16c4ee572707ca7de59b5b7b32b5c8eed
MASHAR27 <iamashar995@gmail.com>
2024-10-12 15:33:42 -0700

    added location

---
+++ my-app/location.py
@@ -0,0 +1,17 @@
+Target:
+Client:
+import http.client
+
+conn = http.client.HTTPSConnection("homeless-shelter.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "dfaf4b8a04mshe5c844193a881dfp1e9589jsn1f70d0548b1d",
+    'x-rapidapi-host': "homeless-shelter.p.rapidapi.com"
+}
+
+conn.request("GET", "/zipcode?zipcode=98004", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
